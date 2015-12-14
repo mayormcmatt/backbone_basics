3bf7f94f912d4bd9c9614d7811fff39f5d9f5ced
mayormcmatt <matthew.is.riding@gmail.com>
2015-12-13 22:42:50 -0800

    hello world done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-main.js
+var AppView = Backbone.View.extend({
+    el: '#contentContainer',
+    initialize: function() {
+        this.render();
+    },
+    render: function() {
+        this.$el.html("Hello World");
+    }
+});
+
+var appView = new AppView();
